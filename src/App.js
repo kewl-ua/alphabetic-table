f61d7866f5e9920d0925fa22b118f030ca7df5d4
kewl-ua <artemdudka1972@gmail.com>
2025-10-19 11:02:45 +0300

    Updated up to 2025.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React, { Suspense } from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
 import store from './redux/store';
-
-const Home = React.lazy(() => import('./pages/Home'));
-const Employees = React.lazy(() => import('./pages/Employees'));
-const NotFound = React.lazy(() => import('./pages/NotFound'));
+import router from './router';
 
 const App = () => {
   return (
-    <div className="App">
-      <Provider store={store}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Router>
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-
-              <Route exact path="/employees">
-                <Employees />
-              </Route>
-
-              <Route status={404}>
-                <NotFound />
-              </Route>
-            </Switch>
-          </Router>
-        </Suspense>
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <RouterProvider router={router} fallbackElement={<div>Loading...</div>} />
+    </Provider>
   );
 };
 
